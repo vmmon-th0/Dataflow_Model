f8b664563a495bc9b39382c00fec714571ddccf5
vmmon-th0 <ammonschiffer@outlook.com>
2024-10-30 00:32:44 +0100

    feat: spring boot dataflow init

---
+++ src/test/java/com/example/dataflowspringboot/DataflowSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataflowspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataflowSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
